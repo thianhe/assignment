0e28487861637be87637d467163d3d77764145c0
thianhe <thianhe@hotmail.com>
2017-10-27 23:21:26 +0800

    ench

--- C/023/023.c
+++ C/023/023.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
 int main() {
 	int w = 0, x = 0, y = 0, z = 0;
+	int a = 0, b = 0, c = 0, d = 0;
+	int i=0;
 	scanf("%d %d %d %d", &w, &x, &y, &z);
-	printf("L");
+	scanf("%d %d %d %d", &a, &b, &c, &d);
+	for(i=0;i>=3;i++){
+
+	}
 }
